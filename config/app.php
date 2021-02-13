0d001e4a1f129ade495f3ead94b3db748e1f8ec9
vagrant <vagrant@eaglezzzzz@rambler.ru>
2021-02-13 12:03:34 +0000

    ready to dev

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Additional Service Providers...
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
